b05427d3fea2d6aa1ce90abb6b94c5677b763723
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2023-05-07 20:09:47 +0800

    Add files via upload

---
+++ sqlite/update_sqlite_action.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#加载公共函数
+source "`pwd`/util_function.sh"
+
+#定义下载目录
+Download_Rules="`pwd`/download"
+
+#清理临时文件
+clean_up_tmpfile
+
+#下载规则
+download_link "https://raw.fgit.ml/lingeringsound/adblock_auto/main/Rules/adblock_auto_lite.txt" "${Download_Rules}"
+
+#读取下载好的Adblock规则
+for file in "${Download_Rules}"/*.txt
+do
+	mkdir_css_Rules "${file}"
+	mkdir_domain_Rules "${file}"
+done
+
+#生成sqlite文件
+python "`pwd`/sqlite.py"
+
+rm -rf "`pwd`/download" "`pwd`/output"
+
+
+
